ae5d5d505570930b302be7d24ba21a6b1c654e55
Akshay Sharma <akshay.sharma979@gmail.com>
2021-05-24 11:27:29 +0530

    Updated Assertions

--- src/test/java/com/learning/SampleTest.java
+++ src/test/java/com/learning/SampleTest.java
@@ -1,18 +1,15 @@
 package com.learning;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SampleTest {
 
 	@Test
 	public void testProgram() {
-		String someWorld = "Worlds";
+		String someWorld = "World";
 
-		if ("World".equalsIgnoreCase(someWorld)) {
-			System.out.println("Equals");
-		} else {
-			System.out.println("Not Equals");
-		}
+		Assert.assertEquals("World", someWorld);
 	}
 
 }
